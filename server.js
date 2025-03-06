95e7a7321c2110c2bf66bbe9023c8ede68880583
GMolter <gmolter8@gmail.com>
2025-03-06 12:13:26 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,46 +3,16 @@ const express = require("express");
 
 const PORT = process.env.PORT || 10000;
 const app = express();
-const server = app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
+const server = app.listen(PORT, () => console.log(`Server on ${PORT}`));
 const wss = new WebSocket.Server({ server });
 
-// Keep track of scores for Gavin and Carson
-let scoreboard = { Gavin: 0, Carson: 0 };
-
-// Send updated scores to all connected clients
-function broadcastScoreboard() {
-  const data = {
-    type: "scoreUpdate",
-    scoreboard
-  };
-  wss.clients.forEach((client) => {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(JSON.stringify(data));
-    }
-  });
-}
-
 wss.on("connection", (ws) => {
-  // Send the current scores to the newly connected client
-  ws.send(JSON.stringify({
-    type: "scoreUpdate",
-    scoreboard
-  }));
-
-  // Listen for messages like { action: "increment", player: "Gavin" }
-  ws.on("message", (message) => {
-    try {
-      const data = JSON.parse(message);
-      if (data.action === "increment" && scoreboard.hasOwnProperty(data.player)) {
-        scoreboard[data.player]++;
-        broadcastScoreboard();
-      } else if (data.action === "decrement" && scoreboard.hasOwnProperty(data.player)) {
-        scoreboard[data.player]--;
-        broadcastScoreboard();
+  ws.on("message", (msg) => {
+    // Broadcast to all
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(msg);
       }
-    } catch (e) {
-      console.log("Error parsing message:", e);
-    }
+    });
   });
 });
