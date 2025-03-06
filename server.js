1bcf5f7f0af7c7265834df521ee146641fd8fba9
GMolter <gmolter8@gmail.com>
2025-03-06 12:18:08 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const WebSocket = require("ws");
 const express = require("express");
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3000;
 const app = express();
-const server = app.listen(PORT, () => console.log(`Server on ${PORT}`));
+const server = app.listen(PORT, () => console.log(`Server on port ${PORT}`));
 const wss = new WebSocket.Server({ server });
 
 wss.on("connection", (ws) => {
   ws.on("message", (msg) => {
-    // Broadcast to all
+    // Just broadcast whatever we receive to all clients
     wss.clients.forEach((client) => {
       if (client.readyState === WebSocket.OPEN) {
         client.send(msg);
