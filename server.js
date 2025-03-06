c0adf788474e6a8b4cf573a39a619f3ed3472d98
GMolter <gmolter8@gmail.com>
2025-03-06 10:58:41 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,14 +7,33 @@ const server = app.listen(PORT, () => console.log(`Server running on port ${PORT
 
 const wss = new WebSocket.Server({ server });
 
+// Keep track of each client and their ID
+let clients = new Map();
+
 wss.on("connection", (ws) => {
+  // Generate a unique ID for this new client
+  const id = Math.random().toString(36).substr(2, 9);
+  clients.set(ws, id);
+
+  // Listen for messages from this client
   ws.on("message", (message) => {
-    // Broadcast the message to everyone
+    // message is something like { x: 123, y: 456 }
+    const data = JSON.parse(message);
+
+    // Attach the client’s unique ID
+    data.id = id;
+
+    // Broadcast to ALL connected clients (including the sender)
     wss.clients.forEach((client) => {
       if (client.readyState === WebSocket.OPEN) {
-        client.send(message);
+        client.send(JSON.stringify(data));
       }
     });
+  });
+
+  // If the client disconnects
+  ws.on("close", () => {
+    clients.delete(ws);
   });
 });
 
